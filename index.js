b96f27524e93cae06118c6eccd4ab391a676d2dd
Jean <jcgaribayg02@hotmail.com>
2024-12-01 15:43:11 -0600

    Implementacion de Swagger

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
-require("dotenv").config();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
+require("dotenv").config();
 const gameRoutes = require('./src/routes/gameRoutes');
 
 const app = express();
@@ -8,5 +10,11 @@ const app = express();
 app.use(express.json());
 app.use("/api/games", gameRoutes);
 
+// Ruta para la documentación de Swagger
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+    console.log(`Documentación disponible en http://localhost:${PORT}/api/docs`);
+});
