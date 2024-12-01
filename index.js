a75257addb2e31ea0a1ce6ef33989168a955c693
Jean <jcgaribayg02@hotmail.com>
2024-12-01 15:22:25 -0600

    Version estable sin Swagger

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const cors = require("cors");
 require("dotenv").config();
 
-const gameRoutes = require("./src/routes/gameRoutes");
+const gameRoutes = require('./src/routes/gameRoutes');
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 app.use("/api/games", gameRoutes);
 
